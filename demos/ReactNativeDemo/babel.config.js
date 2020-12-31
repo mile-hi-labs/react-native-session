8a9cdf8289a2d084576133ea253ef08d29681aaf
Eric Hubbell <eric.hubbell@gmail.com>
2020-12-31 14:38:32 -0700

    add lib directory to git

--- demos/ReactNativeDemo/babel.config.js
+++ demos/ReactNativeDemo/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@haul-bundler/babel-preset-react-native'],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
       'module-resolver',
